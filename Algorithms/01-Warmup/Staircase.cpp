1ff4110073d2a6cbbde92aa1fd5604beca400d95
Sudhanshu Singh <sudhanshuthakurr123@gmail.com>
2017-10-19 07:54:51 -0600

    Create Staircase.cpp

---
+++ Algorithms/01-Warmup/Staircase.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cout.width(n);
+        cout << string(i, '#') << endl;
+    }
+    return 0;
+}

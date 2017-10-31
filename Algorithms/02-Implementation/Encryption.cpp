441c8b793c42f388c26fb91ae71a3135a099ddcb
Sudhanshu Singh <sudhanshuthakurr123@gmail.com>
2017-10-31 11:33:27 -0600

    Create Encryption.cpp

---
+++ Algorithms/02-Implementation/Encryption.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+    auto n = s.size();
+    int row = round(sqrt(n));
+    int column;
+    if (row >= sqrt(n))
+        column = row; 
+    else 
+        column = row + 1;
+    for(int j=0;j<column;++j) {
+        for(int i=j; i<n;i+=column)
+            cout << s[i];
+        cout << ' ';
+    }
+    return 0;
+}
